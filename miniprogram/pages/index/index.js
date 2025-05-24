9e8332d8af3d70b84bd081490885429f7f7c73bf
beans1888 <199010900@qq.com>
2025-05-25 07:40:59 +0800

    feat: 添加轮播图功能并初始化云环境

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,11 +1,20 @@
-//Page Object
 Page({
   data: {
-    
+    imageList: [
+      "cloud://cloud1-9guuoeyie2fd8916.636c-cloud1-9guuoeyie2fd8916-1360998817/微信图片_20250525040651.png",
+      "cloud://cloud1-9guuoeyie2fd8916.636c-cloud1-9guuoeyie2fd8916-1360998817/237879f1f43163117b1dc780f7a4f4eb35da00f7_size510_w1080_h608.jfif"
+    ],
+    imagesLoaded: [false, false],
+    currentSwiperIndex: 0
   },
-  //options(Object)
   onLoad: function(options) {
-    
+    if (!wx.cloud) {
+      wx.showToast({
+        title: '请使用2.2.3或以上基础库',
+        icon: 'none'
+      });
+      return;
+    }
   },
   onReady: function() {
     
@@ -31,9 +40,29 @@ Page({
   onPageScroll: function() {
 
   },
-  //item(index,pagePath,text)
   onTabItemTap:function(item) {
 
+  },
+  swiperChange: function(e) {
+    this.setData({
+      currentSwiperIndex: e.detail.current
+    });
+  },
+  imageLoadSuccess: function(e) {
+    const index = e.currentTarget.dataset.index || 0;
+    let imagesLoaded = this.data.imagesLoaded;
+    imagesLoaded[index] = true;
+    this.setData({
+      imagesLoaded: imagesLoaded
+    });
+  },
+  imageLoadError: function(e) {
+    const index = e.currentTarget.dataset.index || 0;
+    console.error('图片加载失败', e, '索引:', index);
+    wx.showToast({
+      title: '图片加载失败',
+      icon: 'none'
+    });
   }
 });
   
