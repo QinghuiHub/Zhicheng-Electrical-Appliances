e93c9ff2bf6e8bfee7af5c2419a8e0ef03f4d2b8
beans1888 <199010900@qq.com>
2025-05-25 21:58:26 +0800

    feat: Initialize the user center page

--- miniprogram/pages/user-center/index.js
+++ miniprogram/pages/user-center/index.js
@@ -5,14 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    userId: '',
+    hasUserInfo: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getUserInfo();
   },
 
   /**
@@ -26,7 +28,89 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
+    // 每次显示页面时检查用户信息是否更新
+    this.getUserInfo();
+  },
 
+  /**
+   * 获取用户信息
+   */
+  getUserInfo() {
+    const userInfo = wx.getStorageSync('userInfo') || {};
+    const userId = wx.getStorageSync('userId') || this.generateUserId();
+    
+    if (userInfo) {
+      this.setData({
+        userInfo,
+        userId,
+        hasUserInfo: Object.keys(userInfo).length > 0
+      });
+    }
+  },
+
+  /**
+   * 生成随机用户ID
+   */
+  generateUserId() {
+    const userId = 'user_' + Math.random().toString(36).substr(2, 9);
+    wx.setStorageSync('userId', userId);
+    return userId;
+  },
+
+  /**
+   * 选择头像
+   */
+  chooseAvatar() {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        const tempFilePath = res.tempFilePaths[0];
+        
+        // 更新本地存储的用户信息
+        const userInfo = wx.getStorageSync('userInfo') || {};
+        userInfo.avatarUrl = tempFilePath;
+        wx.setStorageSync('userInfo', userInfo);
+        
+        // 更新页面数据
+        this.setData({
+          'userInfo.avatarUrl': tempFilePath,
+          hasUserInfo: true
+        });
+        
+        // 这里可以添加上传头像到服务器的代码
+        // this.uploadAvatar(tempFilePath);
+      }
+    });
+  },
+
+  /**
+   * 编辑昵称
+   */
+  editNickname() {
+    wx.showModal({
+      title: '设置昵称',
+      editable: true,
+      placeholderText: '请输入昵称',
+      success: (res) => {
+        if (res.confirm && res.content) {
+          // 更新本地存储的用户信息
+          const userInfo = wx.getStorageSync('userInfo') || {};
+          userInfo.nickName = res.content;
+          wx.setStorageSync('userInfo', userInfo);
+          
+          // 更新页面数据
+          this.setData({
+            'userInfo.nickName': res.content,
+            hasUserInfo: true
+          });
+          
+          // 这里可以添加上传昵称到服务器的代码
+          // this.updateNickname(res.content);
+        }
+      }
+    });
   },
 
   /**
